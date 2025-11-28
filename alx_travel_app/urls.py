f8f0cd89de34567759c156b5319b7a86f4a24849
Tsionalemu <tsionalemuta@gmail.com>
2025-11-28 10:03:47 +0300

    Fix typo in urls.py and validate required files

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
 # Swagger URLs
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('swagger.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),O
+    path('swagger.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 
 ]
